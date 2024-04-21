58f3bace59c36f6275cdaf9f71026649ce42d4a3
German Sanz <germansanz93@gmail.com>
2024-04-20 23:33:31 -0300

    get all users endpoint

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type User struct {
+	ID        uint64 `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"las_name"`
+	Email     string `json:"email"`
+}
+
+var users []User
+
+func init() { //La funcion init nos sirve para inicializar valores, si nosotros en un package tenemos la funcion init, esto es lo primero que se ejecuta.
+	users = []User{
+		{
+			ID:        1,
+			FirstName: "German",
+			LastName:  "Sanz",
+			Email:     "germansanz@gmail.com",
+		},
+		{
+			ID:        2,
+			FirstName: "Jhon",
+			LastName:  "Doe",
+			Email:     "johndoe@gmail.com",
+		},
+		{
+			ID:        3,
+			FirstName: "Mister",
+			LastName:  "Jagger",
+			Email:     "jaggermister@gmail.com",
+		},
+	}
+}
 
 func main() {
 	http.HandleFunc("/users", UserServer)
@@ -15,9 +48,7 @@ func UserServer(w http.ResponseWriter, r *http.Request) {
 	var status int
 	switch r.Method {
 	case http.MethodGet:
-		status = 200
-		w.WriteHeader(status)
-		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "success in get")
+		GetAllUsers(w)
 	case http.MethodPost:
 		status = 200
 		w.WriteHeader(status)
@@ -27,4 +58,14 @@ func UserServer(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, status, "not found")
 	}
+}
+
+func GetAllUsers(w http.ResponseWriter) {
+	DataResponse(w, http.StatusOK, users)
+}
+
+func DataResponse(w http.ResponseWriter, status int, users interface{}) {
+	value, _ := json.Marshal(users)
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{"status": %d, "data": %s}`, status, value)
 }
