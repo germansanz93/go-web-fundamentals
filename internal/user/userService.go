90f389b29d235f95585e66732cf0033115aa7ca4
German Sanz <germansanz93@gmail.com>
2024-04-21 11:35:23 -0300

    service layer

---
+++ internal/user/userService.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"context"
+	"log"
+
+	"github.com/germansanz93/go-web-fundamentals/internal/user/domain"
+)
+
+type (
+	Service interface {
+		Create(ctx context.Context, firsName, lastName, email string) (*domain.User, error)
+		GetAll(ctx context.Context, firsName, lastName, email string) ([]domain.User, error)
+	}
+
+	service struct {
+		log  *log.Logger
+		repo Repository
+	}
+)
+
+func newService(l *log.Logger, repo Repository) Service {
+	return &service{
+		log:  l,
+		repo: repo,
+	}
+}
+
+func (s service) Create(ctx context.Context, firsName, lastName, email string) (*domain.User, error) {
+	user := &domain.User{
+		FirstName: firsName,
+		LastName:  lastName,
+		Email:     email,
+	}
+	if err := s.repo.Create(ctx, user); err != nil {
+		return nil, err
+	}
+	s.log.Println("service: user create")
+	return user, nil //Tener en cuenta que al estar laburando con puntero, al el repository modificarlo, estaremos devolviendo el modificado
+}
+
+func (s service) GetAll(ctx context.Context, firsName, lastName, email string) ([]domain.User, error) {
+	users, err := s.repo.GetAll(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+	s.log.Println("service: users get all")
+	return users, nil
+}
